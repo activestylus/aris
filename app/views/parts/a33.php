46f0ce23568ea6a45c1eb9f85d0728334f948a2d
Steven Garcia <homebass@Stevens-MacBook-Pro.local>
2024-11-11 19:19:45 +0100

    debugged for production

--- app/views/parts/a33.php
+++ app/views/parts/a33.php
@@ -2,18 +2,16 @@
 $css = <<<EOT
 .a33 {padding-top:25%;  border-radius:4px}
 .a33:hover strong {color:black; text-decoration:underline}
-EOT; 
+EOT;
 yieldStyle($css);
 ?>
 <div class="group flex bac max p16">
-<?php foreach ($group as $g): ?>
-  <?php $f = str_replace(' ', '', $g[0]); ?>
-  
-  <a href="<?= $g[1] ?>" class="a33 lite-px f33 bcov img tdn" style="<?= bg('/i/a50/' . $f . '/960.jpg'); ?>">
-    <span class="db inf bfff rel p4">
-      <strong class="db p8 fs12 fw7"><?= $g[0] ?></strong>
-
-    </span>
-  </a>
-<?php endforeach; ?> 
+  <?php foreach ($group as $g): ?>
+    <?php $f = str_replace(' ', '', $g[0]); ?>
+    <a href="<?= $g[1] ?>" class="a33 lite-px f33 bcov img tdn" style="<?= bg('/i/a50/' . $f . '/960.jpg') ?>">
+      <span class="db inf bfff rel p4">
+        <strong class="db p8 fs12 fw7"><?= $g[0] ?></strong>
+      </span>
+    </a>
+  <?php endforeach; ?>
 </div>
--- app/views/parts/a50.php
+++ app/views/parts/a50.php
@@ -3,13 +3,14 @@ $css = <<<EOT
 .a50 {padding-top:25%;  border-radius:4px}
 .a50 .more {bottom:16px;right:12px}
 .a50 .more:hover {background:#ccc;color:#000}
-EOT; yieldStyle($css) ?>
+EOT; yieldStyle($css)
+?>
 <div class="group flex bac max p16">
 
 <?php foreach ($group as $g):?>
   <?php $f = str_replace(' ','',$g[0]) ?>
   
-  <a href="<?$g[1]?>" class="a50 lite-px f50 bcov img tdn" style="<?= bg('/i/a50/' . $f . '/960.jpg');?>">
+  <a href="<?= $g[1]?>" class="a50 lite-px f50 bcov img tdn" style="<?= bg('/i/a50/' . $f . '/960.jpg');?>">
     <span class="db inf bfff rel p4">
       <strong class="db p8 pb0 fs12 fw7"><?= $g[0] ?></strong>
       <small class="db p8 pt4 fs10"><?= $g[2] ?></small>
